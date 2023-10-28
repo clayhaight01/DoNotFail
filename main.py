1bf2c6552dc5111a5adc646ee291283fb9603da5
Clayton Haight <clayhaight@yahoo.ca>
2023-10-28 16:11:54 -0400

    agent work

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ from readPDF import readPDF
 load_dotenv()
 anthropic = Anthropic()
 
-assignment_txt = readPDF("assignment.pdf")
-submitted_txt = readPDF("submitted.pdf")
-pdf2png("marked.pdf", "marked_png")
-marked_txt = OCR("marked_png")
-print("assignment")
-print(assignment_txt[:1000])
-print("submitted")
-print(submitted_txt[:1000])
-print("submitted")
-print(marked_txt)
-print(type(marked_txt))
+def main():
+    assignment_txt = readPDF("assignment.pdf")
+    submitted_txt = readPDF("submitted.pdf")
+    # pdf2png("marked.pdf", "marked_png")
+    # marked_txt = OCR("marked_png") # replace with GPT4V
 
-# completion = anthropic.completions.create(
-#     model="claude-2",
-#     max_tokens_to_sample=300,
-#     prompt=f"{HUMAN_PROMPT} how does a court case get to the Supreme Court?{AI_PROMPT}",
-# )
-# print(completion.completion)
+    preprompt = HUMAN_PROMPT + "You are in a play where a student at a university gets unfairly marked - it appears that the professor has a vendetta against the student. You are playing the role of the students tutor who has been hired to help negotiate with the TA of the course, you have years of experience. I will play the role of the student in university, I have just submitted an assignment. I have received the marked assignment back from my TA and as expected it is marked very harshly and unfairly. I will give you the assignment, what I submitted, and what I received back. Please help me negotiate with my TA to get a better grade." + AI_PROMPT
+    
+    content = f"Assignment:\n{assignment_txt}\n\nSubmitted:\n{submitted_txt}\n\nMarked:\n{marked_txt}\n\n"
+
+    plan_prompt = HUMAN_PROMPT + preprompt + "First:\nPlease start by reading the assignment, what I submitted, and what I received back. Then, make a plan for where there might be potential to get marks back. This includes things like where I followed the assignment, but got marks off anyway." + AI_PROMPT
+
+    completion = anthropic.completions.create(model="claude-2", max_tokens_to_sample=100000, prompt=preprompt)
+    print("preprompt:")
+    print(completion.completion)
+    plan = anthropic.completions.create(model="claude-2", max_tokens_to_sample=100000, prompt=completion.completion + plan_prompt)
+    print("plan:")
+    print(plan.completion)
+
+if __name__ == "__main__":
+    main()
--- ocr.py
+++ ocr.py
@@ -1,6 +1,22 @@
 import easyocr
 import os
 
+EASY_OCR_PARAMS = {
+    ### TEXT DETECTION PARAMS ###
+    'text_threshold': 0.8,  # Text confidence threshold
+    'low_text': 0.4,        # Text low-bound score
+    'link_threshold': 0.4,  # Link confidence threshold
+    'canvas_size': 2560,    # Maximum image size. Image bigger than this value will be resized down.
+    'mag_ratio': 1,         # Image magnification ratio
+    ### BOUNDING BOX MERGING PARAMS ###
+    'slope_ths': 0.5,       # Maximum slope (delta y/delta x) to considered merging. Low value means tiled boxes will not be merged.
+    'ycenter_ths': 0.5,     # Maximum shift in y direction. Boxes with different level should not be merged.
+    'height_ths': 0.5,      # Maximum different in box height. Boxes with very different text size should not be merged.
+    'width_ths':  0.7,       # Maximum horizontal distance to merge boxes
+    'add_margin': 0.1,      # Extend bounding boxes in all direction by certain value. This is important for language with complex script (E.g. Thai)
+    'x_ths': 2,           # Maximum horizontal distance to merge text boxes when paragraph=True.
+    'y_ths': 0.3            # Maximum vertical distance to merge text boxes when paragraph=True
+}
 reader = easyocr.Reader(['en'], gpu=False, detect_network="craft", recog_network='standard')
 
 def OCR(input_folder):
@@ -9,7 +25,7 @@ def OCR(input_folder):
 
         for index, file_name in enumerate(files, start=1):
             img_path = os.path.join(input_folder, file_name)
-            result = reader.readtext(img_path)
+            result = reader.readtext(img_path, paragraph=True, **EASY_OCR_PARAMS, detail=0)
             
             outfile.write(f"Page {index+1}\n")
             outfile.write("=" * 40)
@@ -22,3 +38,5 @@ def OCR(input_folder):
             outfile.write("\n")
     with open("marked_text.txt", "r", encoding="utf-8") as infile:
         return infile.read()
+
+
