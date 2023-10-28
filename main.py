7c57763dff7206c41f9c9d2f79829e8cbeb8207d
EthChil <ethanchilderhose@gmail.com>
2023-10-28 12:47:07 -0400

    hiya

---
+++ main.py
@@ -0,0 +1,14 @@
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+import os
+
+anthropic = Anthropic(
+    api_key=os.environ.get("ANTHROPIC_API_KEY")
+)
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+
+completion = anthropic.completions.create(
+    model="claude-2",
+    max_tokens_to_sample=300,
+    prompt=f"{HUMAN_PROMPT} how does a court case get to the Supreme Court?{AI_PROMPT}",
+)
+print(completion.completion)
