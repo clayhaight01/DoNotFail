432dbdb781b1e1c72403506ad7a04549baea7c54
Clayton Haight <clayhaight@yahoo.ca>
2023-10-28 13:11:35 -0400

    pdf

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from dotenv import load_dotenv
+import easyocr
+reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
+result = reader.readtext('marked.pdf')
 load_dotenv()
 
-anthropic = Anthropic(
-    # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key=os.environ.get("ANTHROPIC_API_KEY"),
-)
+anthropic = Anthropic()
 
 completion = anthropic.completions.create(
     model="claude-2",
