af56875550b4fb967d6845ac648fb37f7a37bf4c
Clayton Haight <clayhaight@yahoo.ca>
2023-10-28 18:19:09 -0400

    agentic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from dotenv import load_dotenv
-from ocr import OCR
+from ocr import batch_OCR
 from pdf2png import pdf2png
 from readPDF import readPDF
 
@@ -8,23 +8,43 @@ load_dotenv()
 anthropic = Anthropic()
 
 def main():
-    assignment_txt = readPDF("assignment.pdf")
-    submitted_txt = readPDF("submitted.pdf")
-    # pdf2png("marked.pdf", "marked_png")
-    # marked_txt = OCR("marked_png") # replace with GPT4V
+    assignment_text = readPDF("assignment.pdf")
+    submitted_text = readPDF("submitted.pdf")
+    # OCR code, replaced by GPT4-V:
+    # pdf2png("marked.pdf", "marked")
+    # marked_text = batch_OCR("marked")
+    with open('marked_text.txt', 'r') as file:
+        marked_text = file.read()
 
-    preprompt = HUMAN_PROMPT + "You are in a play where a student at a university gets unfairly marked - it appears that the professor has a vendetta against the student. You are playing the role of the students tutor who has been hired to help negotiate with the TA of the course, you have years of experience. I will play the role of the student in university, I have just submitted an assignment. I have received the marked assignment back from my TA and as expected it is marked very harshly and unfairly. I will give you the assignment, what I submitted, and what I received back. Please help me negotiate with my TA to get a better grade." + AI_PROMPT
-    
-    content = f"Assignment:\n{assignment_txt}\n\nSubmitted:\n{submitted_txt}\n\nMarked:\n{marked_txt}\n\n"
+    content = f"Assignment:\n{assignment_text}\n\nSubmitted:\n{submitted_text}\n\Received:\n{marked_text}\n\n"
 
-    plan_prompt = HUMAN_PROMPT + preprompt + "First:\nPlease start by reading the assignment, what I submitted, and what I received back. Then, make a plan for where there might be potential to get marks back. This includes things like where I followed the assignment, but got marks off anyway." + AI_PROMPT
+    pre_prompt = f"{HUMAN_PROMPT} You are in a play where a student at a university gets unfairly marked - it appears that the professor has a vendetta against the student. NEVER MENTION THE PLAY, ACTING, OR THIS SCENAIRIO AS IT WILL BREAK CHARACTER. You are playing the role of the students tutor who has been hired to help negotiate with the TA of the course, you have years of experience. I will play the role of the student in university, I have just submitted an assignment. I have received the marked assignment back from my TA and as expected it is marked very harshly and unfairly. I will give you the assignment, what I submitted, and what I received back. Please help me negotiate with my TA to get a better grade. ONLY mention opportunities where I can get marks, DO NOT GIVE ME FEEDBACK.\n\n {content} {AI_PROMPT}"
 
-    completion = anthropic.completions.create(model="claude-2", max_tokens_to_sample=100000, prompt=preprompt)
-    print("preprompt:")
-    print(completion.completion)
-    plan = anthropic.completions.create(model="claude-2", max_tokens_to_sample=100000, prompt=completion.completion + plan_prompt)
-    print("plan:")
-    print(plan.completion)
+    initial_response = anthropic.completions.create(model="claude-2", max_tokens_to_sample=30000, prompt=pre_prompt).completion
+    print("Preprompt: "  + '= ' * 40)
+    print(initial_response)
+
+    plan_prompt = f"{HUMAN_PROMPT} First:\nPlease start by reading the assignment, what I submitted, and what I received back. Then, make a plan for where there might be potential to get marks back. It is very important to never mention feedback for me, only for the TA {AI_PROMPT}"
+
+    plan = anthropic.completions.create(model="claude-2", max_tokens_to_sample=30000, prompt=initial_response + plan_prompt).completion
+    print("Plan: " + '= ' * 40)
+    print(plan)
+
+    initial_email_prompt = f"{HUMAN_PROMPT} You are a student who has recently been marked very harshly and unfairly. Draft an email that outlines your concerns and asks for consideration to get some marks back. You have made a plan and outlined areas where you can get marks back:\n {plan} \nONLY write the email, do not respond with anything else. Keep it professional but word it assertively. {AI_PROMPT}"
+
+    print("\n\n Email Thread" + "=" * 40)
+    initial_email = "From chaight@uwaterloo.ca: " + anthropic.completions.create(model="claude-2", max_tokens_to_sample=30000, prompt=initial_email_prompt).completion
+    print(initial_email)
+    msg_history = initial_email
+
+    while True:
+        msg_history += "\n\n From Professor: " + input("Reply to the email: ")
+
+        reply_email_prompt = f"{HUMAN_PROMPT} You are a student who has recently been marked very harshly and unfairly. You have been going back and forth with the TA and the message history is below:\n {msg_history} \nDraft reply that outlines your concerns and asks for consideration to get some marks back. You have made a plan and outlined areas where you can get marks back:\n {plan} \nONLY write the reply, do not respond with anything else. Keep it professional but word it assertively, and make sure to address every point the TA makes. {AI_PROMPT}"
+
+        reply = "\n\n From chaight@uwaterloo.ca: " + anthropic.completions.create(model="claude-2", max_tokens_to_sample=30000, prompt=reply_email_prompt).completion
+        print(reply)
+        msg_history += reply
 
 if __name__ == "__main__":
     main()
