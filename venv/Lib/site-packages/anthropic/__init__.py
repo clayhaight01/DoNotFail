85b72c54edafad64e7fa8bad8583a2240106d426
Clayton Haight <clayhaight@yahoo.ca>
2023-10-28 12:24:29 -0400

    anthrobic

---
+++ venv/Lib/site-packages/anthropic/__init__.py
@@ -0,0 +1,85 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from . import types
+from ._types import NoneType, Transport, ProxiesTypes
+from ._utils import file_from_path
+from ._client import (
+    Client,
+    Stream,
+    Timeout,
+    Anthropic,
+    Transport,
+    AsyncClient,
+    AsyncStream,
+    AsyncAnthropic,
+    RequestOptions,
+)
+from ._version import __title__, __version__
+from ._constants import AI_PROMPT as AI_PROMPT
+from ._constants import HUMAN_PROMPT as HUMAN_PROMPT
+from ._exceptions import (
+    APIError,
+    ConflictError,
+    NotFoundError,
+    AnthropicError,
+    APIStatusError,
+    RateLimitError,
+    APITimeoutError,
+    BadRequestError,
+    APIConnectionError,
+    AuthenticationError,
+    InternalServerError,
+    PermissionDeniedError,
+    UnprocessableEntityError,
+    APIResponseValidationError,
+)
+from ._utils._logs import setup_logging as _setup_logging
+
+__all__ = [
+    "types",
+    "__version__",
+    "__title__",
+    "NoneType",
+    "Transport",
+    "ProxiesTypes",
+    "AnthropicError",
+    "APIError",
+    "APIStatusError",
+    "APITimeoutError",
+    "APIConnectionError",
+    "APIResponseValidationError",
+    "BadRequestError",
+    "AuthenticationError",
+    "PermissionDeniedError",
+    "NotFoundError",
+    "ConflictError",
+    "UnprocessableEntityError",
+    "RateLimitError",
+    "InternalServerError",
+    "Timeout",
+    "RequestOptions",
+    "Client",
+    "AsyncClient",
+    "Stream",
+    "AsyncStream",
+    "Anthropic",
+    "AsyncAnthropic",
+    "file_from_path",
+    "HUMAN_PROMPT",
+    "AI_PROMPT",
+]
+
+_setup_logging()
+
+# Update the __module__ attribute for exported symbols so that
+# error messages point to this module instead of the module
+# it was originally defined in, e.g.
+# anthropic._exceptions.NotFoundError -> anthropic.NotFoundError
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        try:
+            setattr(__locals[__name], "__module__", "anthropic")
+        except (TypeError, AttributeError):
+            # Some of our exported symbols are builtins which we can't set attributes for.
+            pass
